e7b24c85e5f40cfc83ad18a69a8c3536637010e6
Jake Soenneker <jake@soenneker.com>
2026-04-21 19:36:23 -0500

    cleanup

--- test/Soenneker.Maf.Pool.Ollama.Tests/MafPoolOllamaExtensionTests.cs
+++ test/Soenneker.Maf.Pool.Ollama.Tests/MafPoolOllamaExtensionTests.cs
@@ -1,16 +1,10 @@
 
 namespace Soenneker.Maf.Pool.Ollama.Tests;
 
-[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
 public sealed class MafPoolOllamaExtensionTests
 {
-    public MafPoolOllamaExtensionTests( output)
-    {
-    }
-
     [Test]
     public void Default()
     {
-
     }
 }
